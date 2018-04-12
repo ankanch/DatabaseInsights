8c9cb0b055a0ef0f0568c713ef34551059db9a91
Long Zhang (Kanch) <kanchisme@gmail.com>
2018-04-12 15:11:50 +0800

    refine the localization generation tool. dlete redunlunt code. Now you can simply add language xml files in dib.localization.langs package, the run generation tool, it will generate automatically. Besides, you have to manually add object refenerce in detectLang() function of UserMnagaer class

--- tools/langStringGnerateTool.py
+++ tools/langStringGnerateTool.py
@@ -5,6 +5,7 @@
 #
 import xml.etree.ElementTree
 import os
+from datetime import datetime
 """
  * The MIT License
  *
@@ -30,15 +31,18 @@ import os
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  """
-print("\n\n\tDatabase Insights Localization Generate Tool\n\t\t\thttp://akakanch.com\n\n\n")
- # target language Java class dir
+print("\n\n\tDatabase Insights Localization Generation Tool\n\t\t\thttp://akakanch.com\n\n\n")
 
-dir_langPackage = "../DatabaseInsights/src/java/dbi/localization/"
-dir_gen_langID = dir_langPackage + "langID.java"
-dir_gen_langEN = dir_langPackage + "langEnglish.java"
-dir_gen_langCN = dir_langPackage + "langChinese.java"
-dir_langString = dir_langPackage + "lang.xml"
-dir_langID = dir_langPackage + "langID.xml"
+# file name map between *.java and *.xml
+# chinese.xml -----> langChinese.java 
+ 
+# target language files dir
+dir_template = "./code_templates/langClass.template"
+dir_templateID = "./code_templates/langID.template"
+dir_langPackage = "../DatabaseInsights/src/java/dbi/localization/"      # localization main folder
+dir_langIDJava = dir_langPackage + "langID.java"
+dir_langSrc = dir_langPackage + "langs/"                                # XML files of different languages
+                                                                        # file name e.g. chinese.xml
 
 sym_beg = "//@DBI_LANGGEN_TOOL_BEGIN"
 sym_end = "//@DBI_LANGGEN_TOOL_END"
@@ -47,66 +51,89 @@ genLangTemplate = "result.put(langID.%s, \"%s\");"
 genIDTemplate = "public static int %s = %s;"
 
 OP = "DILGT|"
+SOP = "\t  |-"
 
-def insertCode(source,target):
-    """
-    Insert target code into source file, based on <sym_beg> and <sym_end>
-    """
-    with open(source,"r+",encoding="utf-8") as f:
+def load_template(path):
+    head = ""
+    tail = ""
+    with open(path,"r",encoding="utf-8") as f:
         origin = f.read()
         news = origin[:origin.find(sym_beg)+len(sym_beg)]
         news += "\r\n\t"
-        news += target
-        news += "\r\n\t"
-        news += origin[origin.find(sym_end):]
-        f.seek(0,0)
+        head = news
+        tail = "\r\n\t" + origin[origin.find(sym_end):]
+        return head,tail
+
+def generateCode(source,target,head="",tail=""):
+    """
+    generate java code from tamplate with <source> based on <sym_beg> and <sym_end>.
+    save java code to target file in <dir_langPackage> folder
+    """
+    with open( dir_langPackage + target ,"w",encoding="utf-8") as f:
         f.truncate()
+        news = head + source + tail
+        news = news.replace("@LANGUAGE_NAME",target[4:target.find('.')]).replace("@CREATE_TIME",str(datetime.now()))
         f.write(news)
         return  news
     return "error"
+
+# load template
+print(OP + "INFO|load template.")
+temp_head,temp_tail = load_template(dir_template)
+temp_head_id,temp_tail_id = load_template(dir_templateID)
+
+
 # start generate
+# list language files
+print(OP + "INFO|listing language files...")
+langxmls = os.listdir(dir_langSrc)
+print(SOP,"INFO|Found:")
+for n in langxmls:
+    print(SOP,"\t-",n)
 
-# read string xml
-print(OP + "INFO|reading lange set from " + dir_langString)
-langset = xml.etree.ElementTree.parse(dir_langString).getroot()
-print(OP + "INFO|reading lange ID set from " + dir_langID)
-langidset = xml.etree.ElementTree.parse(dir_langID).getroot()
+# start reading lang xmls and processing 
+idx = 2000         # ID sequence
+dict_keys = {}
+dict_keysref = {}
+record_id = True
+print(OP + "INFO|Start processing..")
+for langxml in langxmls:
+    print(OP + "INFO|Generting ",langxml,"...")
+    
+    # read and parse xml
+    langset = xml.etree.ElementTree.parse(dir_langSrc + langxml).getroot()
 
-# parse lang ID set
-print(OP + "INFO|parsing... ")
-lidset = langidset.findall('l')
-lset = langset.findall('l')
+    # parse lang ID set
+    print(SOP,"INFO|parsing... ")
+    lset = langset.findall('l')
 
-# generate
-print(OP + "INFO|generating... ")
-langsEN = []
-langsCN = []
-langids = []
+    # record all ids then generate static integer
+    if record_id:
+        keys = [ l.get("id") for l in lset ]
+        for k in keys:
+            dict_keys[k] = idx
+            dict_keysref[k] = 1
+            idx += 1
+        record_id = False
 
-print(OP + "INFO|generating strings... ",end="\t")
-for l in lset:
-    code = genLangTemplate%( l.get("id"), l.get("value") )
-    langswitch = l.get("lang")
-    if langswitch == "cn":
-        langsCN.append(code)
-    elif langswitch == "en":
-        langsEN.append(code)
-print("-done")
+    # generate
+    print(SOP,"INFO|generating strings... ",end="\t")
+    lang = [ genLangTemplate%( l.get("id"), l.get("value") ) for l in lset ]
+    print("-done")  
 
-print(OP + "INFO|generating ids... ",end="\t")
-langids = [ genIDTemplate%( l.get("name"), l.get("id") ) for l in lidset ]
-print("-done")
+    # generate source code
+    langstr = "\n\t".join(lang)
+    print(SOP,"INFO|generating Java code...")
+    fname = "lang" + langxml[:1].upper() + langxml[1:langxml.find(".")] + ".java"
+    generateCode(langstr, fname ,temp_head,temp_tail)
+    print(SOP,"INFO|",langxml,">done.")
 
-# join as strings
-langCNstr = "\n\t".join(langsCN)
-langENstr = "\n\t".join(langsEN)
-langidstr = "\n\t".join(langids)
-
-# insert into source code
-print(OP + "INFO|generating Java code...")
-insertCode(dir_gen_langID,langidstr)
-insertCode(dir_gen_langEN,langENstr)
-insertCode(dir_gen_langCN,langCNstr)
+# generate langID.java
+print(OP,"INFO|Generating langID.java...")
+langIDs = ""
+for k in dict_keys.keys():
+    langIDs += "\n\t" +  genIDTemplate%(k,dict_keys[k])
+generateCode(langIDs, "langID.java" ,temp_head_id,temp_tail_id)
 
 print(OP + "INFO|Code has been generated successfully!\n\n")
 
